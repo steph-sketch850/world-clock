9617c4d379800ef57ac72504d0442ed18ed9398d
steph-sketch850 <145380074+steph-sketch850@users.noreply.github.com>
2024-07-07 13:22:31 -0400

    Double checked function code

--- javascript/main.js
+++ javascript/main.js
@@ -27,9 +27,6 @@ function updateTime() {
 }
 function updateCity(event) {
   let cityTimeZone = event.target.value;
-  if (cityTimeZone === "current") {
-    cityTimeZone = moment.tz.guess();
-  }
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
@@ -37,7 +34,7 @@ function updateCity(event) {
   <div class="city">
     <div>
       <h2>${cityName}</h2>
-      <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+      <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
     </div>
     <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
     "A"
@@ -45,7 +42,6 @@ function updateCity(event) {
   </div>
   `;
 }
-
 let citiesSelectElement = document.querySelector("#city");
 citiesSelectElement.addEventListener("change", updateCity);
 
